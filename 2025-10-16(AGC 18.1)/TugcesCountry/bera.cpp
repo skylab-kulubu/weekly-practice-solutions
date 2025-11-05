08402a5119e203de10bd6dc3d994d389dc9da834
Bera <154339013+fbera231@users.noreply.github.com>
2025-11-05 11:34:19 +0300

    add dfs code with global variables

--- 2025-10-16(AGC 18.1)/TugcesCountry/bera.cpp
+++ 2025-10-16(AGC 18.1)/TugcesCountry/bera.cpp
@@ -1,44 +1,42 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-void DFS(int i,vector<vector<int>>&adj,vector<bool>&visited){
-    visited[i] = true;
-
-    for(auto j : adj[i]){
-        if(!visited[j]){
-            DFS(j,adj,visited);
-        }
-        
+const int N=100001;
+ bool visited[N+1];
+ vector<int> adj[N+1];
+void dfs(int a)
+{
+    visited[a]=1;
+    for(int i : adj[a]){
+        if(!visited[i]) dfs(i);
     }
-    
-}
-
-int dfsOfGraph(vector<vector<int>>& adj){ 
-    int count=0;
-    vector<bool>visited(adj.size(),false);
-    for (int i = 1; i < adj.size(); i++)
-    {
-        if(!visited[i]){ 
-            count++;               
-            DFS(i,adj,visited);
-        }
-        
-    }
-    return count;   
 }
 int main() {
-int M,N;
-cin >> N >> M;
-vector<vector<int>>adj(N+1);
-for (int i = 0; i < M; i++)
-{
-   int u ,v;
-   cin >> u >> v;
-   adj[u].push_back(v);
-   adj[v].push_back(u);
-}
-int ans = dfsOfGraph(adj);
-cout << ans;
+// write your code here
+    int n,m;
+    cin>>n>>m;
+   
+    int numberofCities=0;
+    
+    while(m--)
+    {
+        int x; int y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    
+    int i=1;
+    while(i<=n)
+    {
+        if(!visited[i]){
+        numberofCities++;
+        dfs(i);
+        }
+        i++;
+    }
+    cout<<numberofCities;
+    
+
 	return 0;
 }
